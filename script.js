9329fb3519b87b47cfe5dc5fd1013b8a57d29287
Daniil-Pavlichenko2002 <pavlichenko.02@internet.ru>
2023-04-21 16:54:57 +0300

    сделал адаптив

--- script.js
+++ script.js
@@ -3,20 +3,6 @@ document.querySelector('.header__burger').addEventListener('click', function() {
     document.querySelector('.header__meny').classList.toggle('open');
     document.querySelector('body').classList.toggle('noscroll');
     document.querySelector('.footer').classList.toggle('footer__fixed');
-    document.querySelector('.services__button').classList.toggle('button__fixed');
+    document.querySelector('.project__bottom').classList.toggle('button__fixed');
     })
 
-// let arr = [1, 2, 3 ,4, 5]
-// let newArr = arr.filter((item) => item !== 1 )
-// console.log(newArr)
-
-// let names = 'Вася, Петя, Маша';
-
-// let arr = names.split(', ');
-// console.log(arr )
-
-const str = ["background-color"]
-let f = (str) => {
-  return str.split('')
-}
-f()
